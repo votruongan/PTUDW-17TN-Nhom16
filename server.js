cdd04d98c0945cf5e1df9b5a16dd0fe060c1f315
votruongan <votrngan@hotmail.com>
2020-05-22 18:13:40 +0700

    ADD post-object site

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const app = express()
 const port = 3000
 
 app.use(express.static(__dirname + '/assets/'));
+
 app.get('/', (req, res) =>{
 	res.sendFile(__dirname + '/html/index.html');
 })
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+app.get('/post-object', (req, res) =>{
+	res.sendFile(__dirname + '/html/post-object.html');
+})
+
+app.listen(port, () => console.log(`TUDO app is listening at http://localhost:${port}`))
