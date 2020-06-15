2483a5f68afa0c35bf8ae247942252b861e179bf
votruongan <votrngan@hotmail.com>
2020-06-15 18:27:42 +0700

    Update special font and server port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var favicon = require('serve-favicon');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(favicon("assets/img/fav.png"));
