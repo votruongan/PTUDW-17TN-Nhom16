587d9829f56e562d086b2e477dd64dbf4dc425a1
An <votrngan@hotmail.com>
2020-08-12 01:48:40 +0700

    sync code

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -1,8 +1,10 @@
+
+
 sendBookingRequest.onclick = ()=>{
 	let x = postXHR("book-item","1/1")
 	const obj = {}
 	obj.userId = 123;
-	obj.message = "abc";
+	obj.message = requestMessage.value;
 	x.send(makeFormData(obj));
 }
 
