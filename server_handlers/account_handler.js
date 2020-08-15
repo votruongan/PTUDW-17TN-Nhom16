a7002bf7b15bcf5da91cc15ac9ebf62ff643dc90
hieutdt <hieutdt@vng.com.vn>
2020-08-15 15:14:16 +0700

    Update account_handler.js

--- server_handlers/account_handler.js
+++ server_handlers/account_handler.js
@@ -1,61 +1,77 @@
 const dbHelper = require("./database_helper");
-const util = require('util');
-
-const promiseFind = util.promisify(dbHelper.findDocument);
 
 const userCollection = "Users";
 
-async function isValidUsername(username) {
-	let res = await promiseFind("Users", {username}).catch((e)=>console.log(e));
+class UserHandler {
+	static isValidUsername = async function(username) {
+		let res = await dbHelper.findDocument("Users", {username}).catch((e)=> {
+			console.log(e);
+		});
+		console.log("res = ", res);
+	
+		if (res && res.length > 0) {
+			return false;
+		}
+		return true;
+	}
+	
+	static isValidPhonenumber = async function(phonenumber) {
+		let res = await dbHelper.findDocument("Users", {"phone" : phonenumber}).catch((err) => {
+			console.log("Err: ", err);
+		});
+	
+		if (res && res.length > 0) {
+			return false;
+		}
+		return true;
+	}
+	
+	static createUser = async function(user) {
+		let res = await dbHelper.insertDocument(userCollection, user);
 
-	console.log("res = ", res);
-
-	if (res) {
+		console.log("insert res = ")
+		if (res) {
+			return true;
+		}
 		return false;
 	}
-	return true;
-}
+	
+	static signUpRequest = async function(username, password, name, phonenumber, address) {
+		let user = {
+			"username" 	: username,
+			"password"	: password,
+			"name"		: name,
+			"phone" 	: phonenumber,
+			"address"	: address
+		};
+	
+		// 1. Check username is valid
+		let validUsername = await isValidUsername(username);
+	
+		console.log("Valid Username: ", validUsername);
+	
+		// 2. Check phonenumber is valid
+		let validPhonenumber= await isValidPhoneNumber(phonenumber);
+	
+		console.log("Valid phonenumber: ", validPhonenumber);
 
-async function isValidPhoneNumber(phonenumber) {
-	let res = await promiseFind("Users", phonenumber).catch((err) => {
-		console.log("Err: ", err);
-	});
+		if (!validUsername) {
+			return 1;
+		}
+		if (!validPhonenumber) {
+			return 2;
+		}
 
-	if (res) {
-		return false;
-	}
-	return true;
-}
+		// 3. If valid, create user
+		
 
-function createUser(user) {
-	dbHelper.insertDocument(userCollection, user);
-}
-
-async function signUpRequest(username, password, name, phonenumber, address) {
-	let user = {
-		"username" 	: username,
-		"password"	: password,
-		"name"		: name,
-		"phone" 	: phonenumber,
-		"address"	: address
-	};
-
-	// 1. Check username is valid
-	let validUsername = await isValidUsername(username);
-
-	console.log("Valid Username: ", validUsername);
-
-	// 2. Check phonenumber is valid
-	let validPhonenumbeer= await isValidPhoneNumber(phonenumber);
-
-	console.log("Valid phonenumber: ", validPhonenumbeer);
-
-	// 3. If valid, create user
-	if (validUsername && validPhonenumbeer) {
-		createUser(user);
-	} else {
-		console.log("TH: Create user failed");
+		let created = await createUser(user)
+		if (created) {
+			return 0; // Success
+		} else {
+			return 3; // Failed
+		}
 	}
 }
 
-signUpRequest("tonhieu", "123", "Hieu", "123", "123");
+module.exports = UserHandler;
