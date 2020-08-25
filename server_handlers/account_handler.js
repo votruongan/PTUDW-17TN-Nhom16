e769b6162fd4b4785bbaecb987beccdc79220790
HieuTDT <hieutdt@vng.com.vn>
2020-08-25 22:54:15 +0700

    Add session handle

--- server_handlers/account_handler.js
+++ server_handlers/account_handler.js
@@ -1,6 +1,18 @@
 const dbHelper = require("./database_helper");
+const e = require("express");
 
 const userCollection = "Users";
+const sessionCollection = "Sessions";
+
+function makeid(length) {
+	var result           = '';
+	var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	var charactersLength = characters.length;
+	for ( var i = 0; i < length; i++ ) {
+	   result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+	return result;
+ }
 
 async function isValid(user) {
 	let res = await dbHelper.findDocument("Users", user).catch((e)=> {
@@ -42,9 +54,44 @@ async function logIn(user) {
 	});
 
 	if (res) {
-		return true; 
+		let token =	makeid(12);
+		let sessionRes = await setSesstion(user.email, token);
+		if (sessionRes) {
+			return true;
+		}
+		return false;
 	}
 	return false;
+}
+
+async function setSesstion(email, token) {
+	let date = new Date();
+	let session = {
+		"email"	: email,
+		"token"	: token,
+		"ts"	: date.getTime()
+	}
+
+	if (UserHandler.isValidToken(email, token)) {
+		let res = dbHelper.updateDocument(sessionCollection, session).catch((err) => {
+			console.log("Update session err = ", err);
+		});
+
+		if (res) {
+			return true;
+		}
+		return false;
+
+	} else {
+		let res = dbHelper.insertDocument(sessionCollection, session).catch((err) => {
+			console.log("Insert session err = ", err);
+		});
+
+		if (res) {
+			return true;
+		}
+		return false;
+	}
 }
 
 // Module class for export
@@ -114,6 +161,41 @@ class UserHandler {
 			return true;
 		}
 		return false;
+	}
+
+	static isValidToken = async function(email, token) {
+		let date = new Date();
+		let session = {
+			"email"	: email,
+			"token"	: token
+		}
+
+		let res = dbHelper.findDocument(sessionCollection, session).catch((err) => {
+			console.log(err);
+		});
+
+		// If this session is found
+		if (res) {
+			let lastLogInTime = res.ts;
+
+			// Check if session is out of date: 3 days
+			if (date.getTime() - lastLogInTime >= 3*24*3600*1000) {
+				// Remove this session in db and return false
+				dbHelper.deleteDocument(sessionCollection, session);
+				return false;
+
+			} else {
+				return true;
+			}
+
+		} else {
+			return false;
+		}
+	}
+	
+	static updateSession = async function(email, token) {
+		let res = await setSesstion(email, token);
+		return res;
 	}
 }
 
