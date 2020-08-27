397d9ec860ad5962dbd207face7eed71cd26bad8
Thành Quý <thanhquy1105@gmail.com>
2020-08-27 22:05:38 +0700

    + Add Post Item + display Item info + stuffHander + fix HTML
    + npm install express  multer

--- assets/js/item_info.js
+++ assets/js/item_info.js
@@ -1,3 +1,55 @@
+var url = window.location.href;
+var i =url.search('/')
+while (i>=0){
+	url = url.substr(i+1,url.length-1);
+	i=url.search('/')
+}
+
+
+async function init(){
+const url1 = "http://localhost:3000" + "/item/id/"+url;	
+const response = await fetch(url1, {
+	method: 'GET',
+});
+
+let r = await response.json();
+
+if (r){
+	console.log(r.name)
+		nameStuff.innerHTML = r.name;
+	if (r.star== undefined){
+		starStuff.innerHTML = "0/5"
+		starStuff.size=15;
+	}
+	else {
+		starStuff.innerHTML = r.star;
+	}
+	if (r.hiring == undefined){
+		hiring.innerHTML="(0 lượt cho thuê)";
+	}
+	else hiring.innerHTML = "(" + r.hiring+ "lượt cho thuê)";
+	cost.innerHTML = r.cost;
+	if (r.path!=undefined&& r.path!=null&&r.path!=''){
+		console.log(r.path);
+		const url = "http://localhost:3000/" +r.path;	
+		imageStuff = document.getElementById("imageStuff");
+		console.log(imageStuff)
+		imageStuff.src = url; 
+	}
+	userName.innerHTML = r.userName;
+	if (r.userHiring!=undefined){
+		userHiring.innerHTML=r.userHiring+ "lượt thuê";
+	}
+	else userHiring.innerHTML= "0 lượt thuê"
+	}
+	if(r.describe!=undefined&&r.describe!='')
+		describe.innerHTML = r.describe;
+	else describe.innerHTML=""
+	sale.innerHTML = r.cost*5/100 +'k';
+}
+init();
+
+
 function initPlaceAutocomplete() {
     var input = document.getElementById("pac-input");
     var autocomplete = new google.maps.places.Autocomplete(intput);
